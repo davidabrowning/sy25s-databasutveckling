e97d899d37979c2eddfbdd1b8b13e654feeafa9b
David Browning <david.a.browning@gmail.com>
2025-03-10 23:37:43 +0100

    Create a tv show database.

---
+++ HemläxaEgenDatabas2/HemläxaEgenDatabas2/Migrations/20250310223144_Fix DbSet declaration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HemläxaEgenDatabas2.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDbSetdeclaration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TVShows",
+                columns: table => new
+                {
+                    TVShowId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Network = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TVShows", x => x.TVShowId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TVShows");
+        }
+    }
+}
---
+++ HemläxaEgenDatabas2/HemläxaEgenDatabas2/TVShow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HemläxaEgenDatabas2
+{
+    internal class TVShow
+    {
+        public int TVShowId { get; set; }
+        public string Name { get; set; } = "";
+        public string Network { get; set; } = "";
+        public TVShow(string name, string network)
+        {
+            Name = name;
+            Network = network;
+        }
+
+        public override string? ToString()
+        {
+            return $"Show: {Name} on {Network} (#{TVShowId})";
+        }
+    }
+}
